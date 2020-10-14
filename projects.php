32ce7a7230538a292d2cfc44eeaa89483eb1f8d2
WilsonKinyua <wilsonkinyuam@gmail.com>
2020-10-14 13:39:27 +0300

    projects page working

--- projects.php
+++ projects.php
@@ -1,5 +1,124 @@
-<?php
-include "includes/header.php"; 
-?>
+<?php include "includes/header.php"; ?>
+<!-- inner banner -->
+<div class="inner-banner-w3lso d-flex flex-column justify-content-center align-items-center" style="background: url('https://cdn.pixabay.com/photo/2016/11/03/15/03/solar-panels-1794467_960_720.jpg') no-repeat center;
+    background-size: cover;
+    min-height: 300px;
+    position: relative;
+    z-index: 0;">
+</div>
+<!-- //inner banner -->
+<!-- breadcrumbs -->
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb d-flex justify-content-center">
+        <li class="breadcrumb-item">
+            <a href="index.php" class="m-0">Home</a>
+        </li>
+        <li class="breadcrumb-item active" aria-current="page">Projects</li>
+       
+    </ol>
+</nav>
+<!-- //breadcrumbs -->
+
+
+<section>
+    <style>
+        .table-responsive h2 {
+            text-transform: uppercase;
+            font-size: 35px;
+            font-weight: 900;
+        }
+
+        .table-responsive h2:hover {
+            color: red;
+        }
+    </style>
+    <div class="container table-responsive py-5">
+        <div class="d-flex justify-content-center m-3">
+            <div>
+            <h4 class="mb-3 text-center">KSPC–Kenya Projects FY 2020-2023</h2>
+            </div>
+            <div class="ml-5">
+            <a href="files/KSPC-KENYA-PROJECTS-2020-2023-1.pdf" class="btn btn-primary btn-sm" >Download KSPC–Kenya Projects FY 2020-2023</a>
+            </div>
+        </div>
+        <table class="table table-bordered table-hover" >
+            <thead class="thead-dark" style="font-size: 15px; font-weight:700; text-transform: uppercase;">
+                <tr>
+                    <th scope="col">Item</th>
+                    <th scope="col">Project</th>
+                    <th scope="col">Scope</th>
+                    <th scope="col">Amount US$</th>
+                    <th scope="col">Duration</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>PV Solar 105MW Power Plant</td>
+                    <td>EPCF of 105MW PV Solar Power Plantin Kenya</td>
+                    <td>50,000,000.00</td>
+                    <td>16 Months</td>
+                </tr>
+                <tr class="table-primary">
+                    <th scope="row">1</th>
+                    <td>Social Mix Housing</td>
+                    <td>Develop 6,000 Middle-Income Units2,000 High-Income Unitsunder the theme of Afro-Asia IndustrialComplex</td>
+                    <td>200,000,000.00</td>
+                    <td>24Months</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Green Medical Hub</td>
+                    <td>Fully-integrated multidisciplinary Specialist Centrelocated at the KonzaTechnopolis.Our aim is to be the leader for value-based, high quality healthcare in the region, and our dedicated team are ever ready to serve the local community and visiting health tourists with quality healthcare services.</td>
+                    <td>100,000,000.00</td>
+                    <td>18Months</td>
+                </tr>
+                <tr class="table-primary">
+                    <th scope="row">4</th>
+                    <td>ICT Hub</td>
+                    <td>To help technology startup companies succeed and, they hope, become the next tech titan like Facebook or Microsoft.</td>
+                    <td>10,000,000.00</td>
+                    <td>12 Months</td>
+                </tr>
+                <tr>
+                    <th scope="row">5</th>
+                    <td>Light Manufacturing</td>
+                    <td>Digital Warehousing, Electrical& Electronic Assembling</td>
+                    <td>40,000,000.00</td>
+                    <td>12 Months</td>
+                </tr>
+                <tr class="table-primary">
+                    <th scope="row">6</th>
+                    <td>Business InnovationIncubation</td>
+                    <td></td>
+                    <td>10,000,000.00</td>
+                    <td>6 Months</td>
+                </tr>
+                <tr>
+                    <th scope="row">7</th>
+                    <td>Entertainment</td>
+                    <td>Disneyland Amusement Park</td>
+                    <td>25,000,000.00</td>
+                    <td>12 Months</td>
+                </tr>
+                <tr class="table-primary">
+                    <th scope="row">8</th>
+                    <td>Training Institute</td>
+                    <td>KSPC –Solar Academy</td>
+                    <td>65,000,000.00</td>
+                    <td>6 Months</td>
+                </tr>
+                <tr class="table-success">
+                    <th scope="row"></th>
+                    <td style="font-weight: 900;">Total</td>
+                    <td></td>
+                    <td style="font-weight: 900;">65,000,000.00</td>
+                    <td style="font-weight: 900;">500,000,000.00</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+</section>
 
 <?php include "includes/footer.php"; ?>
